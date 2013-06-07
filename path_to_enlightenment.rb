9931fa3c15e8d9690d10a43680cbb0c7921d4c72
wrichman <williamrichman@gmail.com>
2013-06-06 23:18:17 -0400

    Separating Responsibility

--- path_to_enlightenment.rb
+++ path_to_enlightenment.rb
@@ -7,19 +7,29 @@ require 'about_objects'
 require 'about_nil'
 require 'about_true_and_false'
 require 'about_arrays'
-require 'about_array_assignment'
-require 'about_hashes'
-require 'about_strings'
-require 'about_symbols'
-require 'about_methods'
+
+# Duncan
+# require 'about_array_assignment'
+# require 'about_hashes'
+# require 'about_strings'
+# require 'about_symbols'
+# require 'about_methods'
+
+# Will
 require 'about_constants'
 require 'about_control_statements'
 require 'about_exceptions'
 require 'about_iteration'
 require 'about_blocks'
-require 'about_classes'
-require 'about_open_classes'
-require 'about_scope'
+
+
+# Duncan
+# require 'about_classes'
+# require 'about_open_classes'
+# require 'about_scope'
+
+
+# Will
 require 'about_inheritance'
 require 'about_modules'
 require 'about_class_methods'
